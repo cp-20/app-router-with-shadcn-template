09d2ba659b12b49c0f0d2f939e539db799e8a55f
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-09-26 21:46:54 +0900

    🔧 vitestの設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
-import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-  plugins: [
-    react({
-      jsxImportSource: '@emotion/react',
-    }),
-  ],
   test: {
     globals: true,
     environment: 'happy-dom', // jsdomの代わりにhappy-domを設定した
     setupFiles: [],
     coverage: {
-      provider: 'c8',
+      provider: 'v8',
     },
     restoreMocks: true, // restore mocks before every tests run
   },
