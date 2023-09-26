d2fdfcc5056c86d46c11ec71a8df5e96993f9359
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-09-27 02:21:51 +0900

    🚧 metadataが正しく設定されるように

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,45 +1,7 @@
 import 'ress';
 import '@/app/globals.css';
-import type { Metadata } from 'next';
 import type { FC, ReactNode } from 'react';
 import { GoogleTagManager } from '@/shared/components/GoogleTagManager';
-
-export const appBaseUrl = 'https://dicespec.vercel.app';
-
-export const metadata: Metadata = {
-  title: {
-    template: '%s - ダイススペック',
-    default: 'ダイススペック',
-  },
-  metadataBase: new URL(appBaseUrl),
-  openGraph: {
-    type: 'website',
-    locale: 'ja',
-    images: `${appBaseUrl}/ogp.png`,
-  },
-  manifest: '/manifest.webmanifest',
-  icons: [
-    {
-      rel: 'icon',
-      url: '/favicon.ico',
-      sizes: 'any',
-    },
-    {
-      rel: 'icon',
-      url: '/icon.svg',
-      type: 'image/svg+xml',
-    },
-    {
-      rel: 'apple-touch-icon',
-      url: '/apple-touch-icon.png',
-    },
-  ],
-  twitter: {
-    card: 'summary_large_image',
-    site: '@__cp20__',
-    images: `${appBaseUrl}/ogp.png`,
-  },
-};
 
 const RootLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
