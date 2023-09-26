fe92a9c8101c1444129e71894bfcea94c1df40cb
cp-20 <47262658+mario-hsp@users.noreply.github.com>
2023-09-26 18:00:19 +0900

    🚧 いろいろ

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import 'ress';
 import '@/shared/styles/global.css';
-import { MantineProvider } from '@mantine/core';
 import type { AppProps } from 'next/app';
 import { GoogleTagManagerBody } from '@/shared/components/GoogleTagManager';
 
@@ -8,23 +7,8 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <GoogleTagManagerBody />
-      <MantineProvider
-        withGlobalStyles
-        withNormalizeCSS
-        theme={{
-          /** Put your mantine theme override here */
-          colorScheme: 'light',
-          breakpoints: {
-            xs: '360px',
-            sm: '800px',
-            md: '1000px',
-            lg: '1200px',
-            xl: '1400px',
-          },
-        }}
-      >
-        <Component {...pageProps} />
-      </MantineProvider>
+
+      <Component {...pageProps} />
     </>
   );
 }
